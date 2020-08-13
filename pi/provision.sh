7750269d412a3d108589ddee919f730ca189f4cd
Henry Cooke <me@prehensile.co.uk>
2020-08-13 15:54:02 +0100

    adding some rpi config

---
+++ pi/provision.sh
@@ -0,0 +1,13 @@
+##
+# Set up a new Raspberry Pi OS installation for Canto.
+#
+
+# make sure package sources are up to date
+sudo apt update
+
+# install python3 and pip3
+sudo apt install python3-pip
+
+# install python neopixel dependencies
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+sudo python3 -m pip install --force-reinstall adafruit-blinka
